98af31554cb6e57c2ae361ceec9df815685f5f5a
wheatup <acphun@gmail.com>
2019-08-27 11:23:07 +0900

    fix cases

--- client/src/components/views/room/Timer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { CSSProperties } from 'react';
-
-const Timer = ({ percentage }) => {
-	return (
-		<div className="Timer" style={{ '--percentage': percentage }}></div>
-	);
-}
-
-export default Timer;
