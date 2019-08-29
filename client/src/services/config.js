b798a73f769c725700f67f549a0485597d7c3950
wheatup <acphun@gmail.com>
2019-08-29 18:02:39 +0900

    config

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -3,7 +3,30 @@ import http from './core/http';
 let config = {};
 
 async function init() {
-	const result = (await http.get('./config.json')).data;
+	let result
+	try {
+		console.log('读取config_override.json成功')
+		result = await http.get('./config_override.json');
+	} catch {
+
+	}
+
+	if (!result) {
+		console.log('无法找到 config_override.json，读取config.json')
+		try {
+			result = await http.get('./config.json');
+		} catch {
+
+		}
+	}
+
+	if (!result) {
+		console.error('无法读取config.json！')
+		return;
+	}
+
+	result = result.data;
+
 	let data = result['default'];
 
 	if (data[window.location.hostname]) {
