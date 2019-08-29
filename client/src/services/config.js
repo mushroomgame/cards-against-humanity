38bbd82274bdbf021484595917c65d34ef08d0f1
wheatup <acphun@gmail.com>
2019-08-29 17:46:57 +0900

    config

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -2,9 +2,15 @@ import http from './core/http';
 
 let config = {};
 
-async function init(){
-	const result = await http.get('./config.json');
-	config = result.data;
+async function init() {
+	const result = (await http.get('./config.json')).data;
+	let data = result['default'];
+
+	if (data[window.location.hostname]) {
+		data = { ...data, ...data[window.location.hostname] };
+	}
+
+	config = data;
 }
 
 function get(key) {
